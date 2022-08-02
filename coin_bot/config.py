c742bedfff84cb3c3f6f9f2be2c30668e7b693e8
Alexander Bozhko <108063065+pers5not@users.noreply.github.com>
2022-08-02 21:53:09 +0300

    delete tgBot Token

--- coin_bot/config.py
+++ coin_bot/config.py
@@ -2,12 +2,5 @@
 
 from enum import Enum
 
-BOT_TOKEN = "5358182286:AAHj5r9YONEUkHe71O9IdGS31H_oGGVPCEU"
-db_file = "db/database_bot_coin.vdb"
+BOT_TOKEN = ""
 
-
-class States(Enum):
-    S_START = "0"  # Начало нового диалога
-    S_YES = "1"
-    S_ENTER_QUANTITY_TOSS = "2"
-    S_ENTER_QUANTITY_EXPERIMENT = "3"
